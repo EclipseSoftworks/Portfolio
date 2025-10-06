aed5e77707b02ef5cd9d0728b5a603fdebdf644b
EclipseSoftworks <eclipsesoftworksltd@gmail.com>
2025-10-06 18:07:03 +0000

    Update engineer.js

--- api/engineer.js
+++ api/engineer.js
@@ -2,11 +2,14 @@ let eclipsecheat = 117006922827147; // stored in memory (resets when Vercel rest
 
 export default function handler(req, res) {
   if (req.method === 'GET') {
-    res.status(200).json({ number });
+    // Send the current number
+    res.status(200).json({ eclipsecheat });
   } else if (req.method === 'POST') {
+    // Update the number
     eclipsecheat = req.body.number;
     res.status(200).json({ message: 'you are a skid', eclipsecheat });
   } else {
+    // Invalid method
     res.status(405).json({ message: 'Method not allowed' });
   }
 }
